d2c41401d68f52049618836125eab4a66f7e33f5
sasedev <sasedev.bifidis@gmail.com>
2016-01-24 23:48:29 +0100

    Added Devis

---
+++ src/Acf/FrontBundle/Controller/DevisController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Acf\FrontBundle\Controller;
+
+use Acf\FrontBundle\Form\Devis\NewTForm as DevisNewTForm;
+use Sasedev\Commons\SharedBundle\Controller\BaseController;
+
+/**
+ *
+ * @author sasedev <seif.salah@gmail.com>
+ */
+class DevisController extends BaseController
+{
+
+	/**
+	 *
+	 * @var array
+	 */
+	protected $gvars = array();
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+
+		$this->gvars['menu_active'] = 'home';
+
+	}
+
+	public function indexAction()
+	{
+		$devisNewForm = $this->createForm(DevisNewTForm::class);
+		$this->gvars['DevisNewForm'] = $devisNewForm->createView();
+
+		return $this->renderResponse('AcfFrontBundle:Default:devis.html.twig', $this->gvars);
+	}
+
+	public function postAction()
+	{
+		$urlFrom = $this->getReferer();
+		if (null == $urlFrom || trim($urlFrom) == '') {
+			$urlFrom = $this->generateUrl('_front_devis');
+		}
+		$devisNewForm = $this->createForm(DevisNewTForm::class);
+
+		$request = $this->getRequest();
+		$reqData = $request->request->all();
+
+		if (isset($reqData['DevisNewForm'])) {
+			$devisNewForm->handleRequest($request);
+			if ($devisNewForm->isValid()) {
+
+				$from = $this->getParameter('mail_from');
+				$fromName = $this->getParameter('mail_from_name');
+				$subject = $this->translate('_mail.devis.subject', array(), 'messages');
+
+				$mvars = array();
+				$mvars['firstname'] = $devisNewForm['firstname']->getData();
+				$mvars['lastname'] = $devisNewForm['lastname']->getData();
+				$mvars['email'] = $devisNewForm['email']->getData();
+				$mvars['phone'] = $devisNewForm['phone']->getData();
+				$mvars['entreprise'] = $devisNewForm['entreprise']->getData();
+				$mvars['entrepriseForm'] = $devisNewForm['entrepriseForm']->getData();
+				$mvars['activity'] = $devisNewForm['activity']->getData();
+				$mvars['dtActivation'] = $devisNewForm['dtActivation']->getData();
+				$mvars['address'] = $devisNewForm['address']->getData();
+				$mvars['zipCode'] = $devisNewForm['zipCode']->getData();
+				$mvars['town'] = $devisNewForm['town']->getData();
+				$mvars['caYear'] = $devisNewForm['caYear']->getData();
+				$mvars['nbrInvoicesBuy'] = $devisNewForm['nbrInvoicesBuy']->getData();
+				$mvars['nbrInvoicesSale'] = $devisNewForm['nbrInvoicesSale']->getData();
+				$mvars['nbrSalaries'] = $devisNewForm['nbrSalaries']->getData();
+				$mvars['hasexpert'] = $devisNewForm['hasexpert']->getData();
+				$mvars['otherInfos'] = $devisNewForm['otherInfos']->getData();
+
+				try {
+					$admins = $this->getParameter('mailtos');
+
+					$message = \Swift_Message::newInstance()->setFrom($from, $fromName);
+					foreach ($admins as $admin) {
+						$message->addTo($admin['email'], $admin['name']);
+						$message->setReplyTo($mvars['email'], $mvars['lastname'].' '.$mvars['firstname']);
+					}
+					$message->setSubject($subject);
+					$message->setBody($this->renderView('AcfFrontBundle:Mail:devis.html.twig', $mvars), 'text/html');
+
+					$this->sendmail($message);
+				} catch (\Exception $e) {
+					$logger = $this->getLogger();
+					$logger->addError($e->getMessage());
+				}
+				$this->flashMsgSession('success', $this->translate('_front.devis.success'));
+
+				return $this->redirect($this->generateUrl('_front_devis'));
+			} else {
+				$this->gvars['DevisNewForm'] = $devisNewForm->createView();
+
+				$this->flashMsgSession('error', $this->translate('_front.devis.error'));
+
+				return $this->renderResponse('AcfFrontBundle:Default:devis.html.twig', $this->gvars);
+			}
+		}
+
+		return $this->redirect($urlFrom);
+	}
+
+}
---
+++ src/Acf/FrontBundle/Form/Devis/NewTForm.php
@@ -0,0 +1,259 @@
+<?php
+
+namespace Acf\FrontBundle\Form\Devis;
+
+use Sasedev\Commons\SharedBundle\Validator\Phone;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
+use Symfony\Component\Validator\Constraints\LessThan;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+/**
+ *
+ * @author sasedev
+ */
+class NewTForm extends AbstractType
+{
+
+	/**
+	 * Form builder
+	 *
+	 * @param FormBuilderInterface $builder
+	 * @param array $options
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder->add('lastname', TextType::class,
+			array(
+				'label' => 'Devis.lastname.label',
+				'mapped' => false,
+				'constraints' => new NotBlank()
+			)
+		);
+
+		$builder->add('firstname', TextType::class,
+			array(
+				'label' => 'Devis.firstname.label',
+				'mapped' => false,
+				'constraints' => new NotBlank()
+			)
+		);
+
+		$builder->add('email', EmailType::class,
+			array(
+				'label' => 'Devis.email.label',
+				'mapped' => false,
+				'constraints' => array(
+					new NotBlank(),
+					new Email(array('checkMX' => true, 'checkHost' => true))
+				)
+			)
+		);
+
+		$builder->add('phone', TextType::class,
+			array(
+				'label' => 'Devis.phone.label',
+				'mapped' => false,
+				'constraints' => array(
+					new NotBlank(),
+					new Phone()
+				)
+			)
+		);
+
+		$builder->add('entreprise', TextType::class,
+			array(
+				'label' => 'Devis.entreprise.label',
+				'mapped' => false,
+				'constraints' => array(
+					new NotBlank()
+				)
+			)
+		);
+
+		$builder->add('entrepriseForm', TextType::class,
+			array(
+				'label' => 'Devis.entrepriseForm.label',
+				'mapped' => false,
+				'constraints' => array(
+					new NotBlank()
+				)
+			)
+		);
+
+		$builder->add('activity', TextType::class,
+			array(
+				'label' => 'Devis.activity.label',
+				'mapped' => false,
+				'constraints' => array(
+					new NotBlank()
+				)
+			)
+		);
+
+
+
+		$builder->add('dtActivation', DateType::class,
+			array(
+				'label' => 'Devis.dtActivation.label',
+				'widget' => 'single_text',
+				'format' => 'yyyy-MM-dd',
+				'mapped' => false,
+				'required' => false,
+				'constraints' => array(
+					new GreaterThanOrEqual(array("value"=>"1950-01-01")),
+					new LessThan((array("value"=>"2100-01-01")))
+				)
+			));
+
+		$builder->add('address', TextareaType::class,
+			array(
+				'label' => 'Devis.address.label',
+				'mapped' => false,
+				'constraints' => array(
+					new NotBlank()
+				)
+			)
+		);
+
+		$builder->add('zipCode', TextType::class,
+			array(
+				'label' => 'Devis.zipCode.label',
+				'mapped' => false,
+				'constraints' => array(
+					new NotBlank()
+				)
+			)
+		);
+
+		$builder->add('town', TextType::class,
+			array(
+				'label' => 'Devis.town.label',
+				'mapped' => false,
+				'constraints' => array(
+					new NotBlank()
+				)
+			)
+		);
+
+		$builder->add('caYear', NumberType::class,
+			array(
+				'label' => 'Devis.caYear.label',
+				'mapped' => false,
+				'required' => false,
+				'constraints' => array(
+					new GreaterThanOrEqual(array("value" => 0))
+				)
+			)
+		);
+
+		$builder->add('nbrInvoicesBuy', IntegerType::class,
+			array(
+				'label' => 'Devis.nbrInvoicesBuy.label',
+				'mapped' => false,
+				'required' => false,
+				'data' => 0,
+				'constraints' => array(
+					new GreaterThanOrEqual(array("value" => 0))
+				)
+			)
+		);
+
+		$builder->add('nbrInvoicesSale', IntegerType::class,
+			array(
+				'label' => 'Devis.nbrInvoicesSale.label',
+				'mapped' => false,
+				'required' => false,
+				'data' => 0,
+				'constraints' => array(
+					new GreaterThanOrEqual(array("value" => 0))
+				)
+			)
+		);
+
+		$builder->add('nbrSalaries', IntegerType::class,
+			array(
+				'label' => 'Devis.nbrSalaries.label',
+				'mapped' => false,
+				'required' => false,
+				'data' => 0,
+				'constraints' => array(
+					new GreaterThanOrEqual(array("value" => 0))
+				)
+			)
+		);
+
+		$builder->add('hasexpert', ChoiceType::class,
+			array(
+				'label' => 'Devis.hasexpert.label',
+				'choices_as_values' => true,
+				'choices' => array(
+					'Devis.hasexpert.1' => 1,
+					'Devis.hasexpert.2' => 2
+				),
+				'expanded' => true,
+				'attr' => array(
+					'choice_label_trans' => true
+				),
+				'mapped' => false
+			)
+		);
+
+		$builder->add('otherInfos', TextareaType::class,
+			array(
+				'label' => 'Devis.otherInfos.label',
+				'required' => false,
+				'mapped' => false
+			)
+		);
+	}
+
+	/**
+	 *
+	 * {@inheritDoc} @see FormTypeInterface::getName()
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 'DevisNewForm';
+	}
+
+	/**
+	 *
+	 * {@inheritDoc} @see AbstractType::getBlockPrefix()
+	 */
+	public function getBlockPrefix()
+	{
+		return $this->getName();
+	}
+
+	/**
+	 * get the default options
+	 *
+	 * @return multitype:string multitype:string
+	 */
+	public function getDefaultOptions()
+	{
+		return array('validation_groups' => array('Default'));
+	}
+
+	/**
+	 *
+	 * {@inheritDoc} @see AbstractType::configureOptions()
+	 */
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults($this->getDefaultOptions());
+	}
+}
