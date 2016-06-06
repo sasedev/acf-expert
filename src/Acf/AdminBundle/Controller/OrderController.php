09a982ce355cfc7912c76bdab05cac250d1731ba
sasedev <sasedev.bifidis@gmail.com>
2016-06-06 13:12:27 +0200

    wrong role check

--- src/Acf/AdminBundle/Controller/OrderController.php
+++ src/Acf/AdminBundle/Controller/OrderController.php
@@ -35,17 +35,17 @@ class OrderController extends BaseController
      */
     public function listAction()
     {
-        if (!$this->hasRole('ROLE_SUPERSUPERADMIN')) {
+        if (!$this->hasRole('ROLE_SUPERADMIN')) {
             return $this->redirect($this->generateUrl('_admin_homepage'));
         }
         $em = $this->getEntityManager();
         $orders = $em->getRepository('AcfDataBundle:Order')->getAll();
         $this->gvars['orders'] = $orders;
-
+        
         $this->gvars['smenu_active'] = 'list';
         $this->gvars['pagetitle'] = $this->translate('pagetitle.order.list');
         $this->gvars['pagetitle_txt'] = $this->translate('pagetitle.order.list.txt');
-
+        
         return $this->renderResponse('AcfAdminBundle:Order:list.html.twig', $this->gvars);
     }
 
@@ -55,18 +55,18 @@ class OrderController extends BaseController
      */
     public function addGetAction()
     {
-        if (!$this->hasRole('ROLE_SUPERSUPERADMIN')) {
+        if (!$this->hasRole('ROLE_SUPERADMIN')) {
             return $this->redirect($this->generateUrl('_admin_homepage'));
         }
         $order = new Order();
         $orderNewForm = $this->createForm(OrderNewTForm::class, $order);
         $this->gvars['order'] = $order;
         $this->gvars['OrderNewForm'] = $orderNewForm->createView();
-
+        
         $this->gvars['pagetitle'] = $this->translate('pagetitle.order.add');
         $this->gvars['pagetitle_txt'] = $this->translate('pagetitle.order.add.txt');
         $this->gvars['smenu_active'] = 'add';
-
+        
         return $this->renderResponse('AcfAdminBundle:Order:add.html.twig', $this->gvars);
     }
 
@@ -76,21 +76,21 @@ class OrderController extends BaseController
      */
     public function addPostAction()
     {
-        if (!$this->hasRole('ROLE_SUPERSUPERADMIN')) {
+        if (!$this->hasRole('ROLE_SUPERADMIN')) {
             return $this->redirect($this->generateUrl('_admin_homepage'));
         }
         $urlFrom = $this->getReferer();
         if (null == $urlFrom || trim($urlFrom) == '') {
             return $this->redirect($this->generateUrl('_admin_order_addGet'));
         }
-
+        
         $order = new Order();
         $orderNewForm = $this->createForm(OrderNewTForm::class, $order);
         $this->gvars['order'] = $order;
-
+        
         $request = $this->getRequest();
         $reqData = $request->request->all();
-
+        
         if (isset($reqData['OrderNewForm'])) {
             $orderNewForm->handleRequest($request);
             if ($orderNewForm->isValid()) {
@@ -100,7 +100,7 @@ class OrderController extends BaseController
                 $this->flashMsgSession('success', $this->translate('Order.add.success', array(
                     '%order%' => $order->getRef()
                 )));
-
+                
                 return $this->redirect($this->generateUrl('_admin_order_editGet', array(
                     'uid' => $order->getId()
                 )));
@@ -109,11 +109,11 @@ class OrderController extends BaseController
             }
         }
         $this->gvars['OrderNewForm'] = $orderNewForm->createView();
-
+        
         $this->gvars['pagetitle'] = $this->translate('pagetitle.order.add');
         $this->gvars['pagetitle_txt'] = $this->translate('pagetitle.order.add.txt');
         $this->gvars['smenu_active'] = 'add';
-
+        
         return $this->renderResponse('AcfAdminBundle:Order:add.html.twig', $this->gvars);
     }
 
@@ -124,7 +124,7 @@ class OrderController extends BaseController
      */
     public function deleteAction($uid)
     {
-        if (!$this->hasRole('ROLE_SUPERSUPERADMIN')) {
+        if (!$this->hasRole('ROLE_SUPERADMIN')) {
             return $this->redirect($this->generateUrl('_admin_homepage'));
         }
         $urlFrom = $this->getReferer();
@@ -134,13 +134,13 @@ class OrderController extends BaseController
         $em = $this->getEntityManager();
         try {
             $order = $em->getRepository('AcfDataBundle:Order')->find($uid);
-
+            
             if (null == $order) {
                 $this->flashMsgSession('warning', $this->translate('Order.delete.notfound'));
             } else {
                 $em->remove($order);
                 $em->flush();
-
+                
                 $this->flashMsgSession('success', $this->translate('Order.delete.success', array(
                     '%order%' => $order->getRef()
                 )));
@@ -148,10 +148,10 @@ class OrderController extends BaseController
         } catch (\Exception $e) {
             $logger = $this->getLogger();
             $logger->addCritical($e->getLine() . ' ' . $e->getMessage() . ' ' . $e->getTraceAsString());
-
+            
             $this->flashMsgSession('error', $this->translate('Order.delete.failure'));
         }
-
+        
         return $this->redirect($urlFrom);
     }
 
@@ -162,47 +162,47 @@ class OrderController extends BaseController
      */
     public function editGetAction($uid)
     {
-        if (!$this->hasRole('ROLE_SUPERSUPERADMIN')) {
+        if (!$this->hasRole('ROLE_SUPERADMIN')) {
             return $this->redirect($this->generateUrl('_admin_homepage'));
         }
         $urlFrom = $this->getReferer();
         if (null == $urlFrom || trim($urlFrom) == '') {
             $urlFrom = $this->generateUrl('_admin_order_list');
         }
-
+        
         $em = $this->getEntityManager();
         try {
             $order = $em->getRepository('AcfDataBundle:Order')->find($uid);
-
+            
             if (null == $order) {
                 $this->flashMsgSession('warning', $this->translate('Order.edit.notfound'));
             } else {
                 $orderUpdateValForm = $this->createForm(OrderUpdateValTForm::class, $order);
                 $orderUpdateStatusForm = $this->createForm(OrderUpdateStatusTForm::class, $order);
                 $orderUpdateDescriptionForm = $this->createForm(OrderUpdateDescriptionTForm::class, $order);
-
+                
                 $this->gvars['order'] = $order;
                 $this->gvars['OrderUpdateValForm'] = $orderUpdateValForm->createView();
                 $this->gvars['OrderUpdateStatusForm'] = $orderUpdateStatusForm->createView();
                 $this->gvars['OrderUpdateDescriptionForm'] = $orderUpdateDescriptionForm->createView();
-
+                
                 $this->gvars['tabActive'] = $this->getSession()->get('tabActive', 1);
                 $this->getSession()->remove('tabActive');
-
+                
                 $this->gvars['pagetitle'] = $this->translate('pagetitle.order.edit', array(
                     '%order%' => $order->getRef()
                 ));
                 $this->gvars['pagetitle_txt'] = $this->translate('pagetitle.order.edit.txt', array(
                     '%order%' => $order->getRef()
                 ));
-
+                
                 return $this->renderResponse('AcfAdminBundle:Order:edit.html.twig', $this->gvars);
             }
         } catch (\Exception $e) {
             $logger = $this->getLogger();
             $logger->addCritical($e->getLine() . ' ' . $e->getMessage() . ' ' . $e->getTraceAsString());
         }
-
+        
         return $this->redirect($urlFrom);
     }
 
@@ -213,31 +213,31 @@ class OrderController extends BaseController
      */
     public function editPostAction($uid)
     {
-        if (!$this->hasRole('ROLE_SUPERSUPERADMIN')) {
+        if (!$this->hasRole('ROLE_SUPERADMIN')) {
             return $this->redirect($this->generateUrl('_admin_homepage'));
         }
         $urlFrom = $this->getReferer();
         if (null == $urlFrom || trim($urlFrom) == '') {
             return $this->redirect($this->generateUrl('_admin_order_list'));
         }
-
+        
         $em = $this->getEntityManager();
         try {
             $order = $em->getRepository('AcfDataBundle:Order')->find($uid);
-
+            
             if (null == $order) {
                 $this->flashMsgSession('warning', $this->translate('Order.edit.notfound'));
             } else {
                 $orderUpdateValForm = $this->createForm(OrderUpdateValTForm::class, $order);
                 $orderUpdateStatusForm = $this->createForm(OrderUpdateStatusTForm::class, $order);
                 $orderUpdateDescriptionForm = $this->createForm(OrderUpdateDescriptionTForm::class, $order);
-
+                
                 $this->gvars['tabActive'] = $this->getSession()->get('tabActive', 2);
                 $this->getSession()->remove('tabActive');
-
+                
                 $request = $this->getRequest();
                 $reqData = $request->request->all();
-
+                
                 if (isset($reqData['OrderUpdateValForm'])) {
                     $this->gvars['tabActive'] = 2;
                     $this->getSession()->set('tabActive', 2);
@@ -248,11 +248,11 @@ class OrderController extends BaseController
                         $this->flashMsgSession('success', $this->translate('Order.edit.success', array(
                             '%order%' => $order->getRef()
                         )));
-
+                        
                         return $this->redirect($urlFrom);
                     } else {
                         $em->refresh($order);
-
+                        
                         $this->flashMsgSession('error', $this->translate('Order.edit.failure', array(
                             '%order%' => $order->getRef()
                         )));
@@ -267,11 +267,11 @@ class OrderController extends BaseController
                         $this->flashMsgSession('success', $this->translate('Order.edit.success', array(
                             '%order%' => $order->getRef()
                         )));
-
+                        
                         return $this->redirect($urlFrom);
                     } else {
                         $em->refresh($order);
-
+                        
                         $this->flashMsgSession('error', $this->translate('Order.edit.failure', array(
                             '%order%' => $order->getRef()
                         )));
@@ -286,36 +286,36 @@ class OrderController extends BaseController
                         $this->flashMsgSession('success', $this->translate('Order.edit.success', array(
                             '%order%' => $order->getRef()
                         )));
-
+                        
                         return $this->redirect($urlFrom);
                     } else {
                         $em->refresh($order);
-
+                        
                         $this->flashMsgSession('error', $this->translate('Order.edit.failure', array(
                             '%order%' => $order->getRef()
                         )));
                     }
                 }
-
+                
                 $this->gvars['order'] = $order;
                 $this->gvars['OrderUpdateValForm'] = $orderUpdateValForm->createView();
                 $this->gvars['OrderUpdateStatusForm'] = $orderUpdateStatusForm->createView();
                 $this->gvars['OrderUpdateDescriptionForm'] = $orderUpdateDescriptionForm->createView();
-
+                
                 $this->gvars['pagetitle'] = $this->translate('pagetitle.order.edit', array(
                     '%order%' => $order->getRef()
                 ));
                 $this->gvars['pagetitle_txt'] = $this->translate('pagetitle.order.edit.txt', array(
                     '%order%' => $order->getRef()
                 ));
-
+                
                 return $this->renderResponse('AcfAdminBundle:Order:edit.html.twig', $this->gvars);
             }
         } catch (\Exception $e) {
             $logger = $this->getLogger();
             $logger->addCritical($e->getLine() . ' ' . $e->getMessage() . ' ' . $e->getTraceAsString());
         }
-
+        
         return $this->redirect($urlFrom);
     }
 }
