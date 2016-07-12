f8ecc1ba32e1d474bef51759e6be4f3af9cdab7c
sasedev <sasedev.bifidis@gmail.com>
2016-07-12 09:53:19 +0200

    disabling HttpException on non concordant status

--- src/Acf/FrontBundle/Controller/PaymentNotifController.php
+++ src/Acf/FrontBundle/Controller/PaymentNotifController.php
@@ -25,25 +25,25 @@ class PaymentNotifController extends BaseController
     public function paymentnotifAction()
     {
         $request = $this->getRequest();
-        
+
         $ipFrom = $request->getClientIp();
         $logger = $this->getLogger();
         $logger->addError('IP : ' . $ipFrom);
-        
+
         if ($ipFrom != '196.203.11.74' && $ipFrom != '196.203.11.69' && $ipFrom != '196.203.11.72') {
             throw new HttpException('403', 'Access Forbiden');
         }
-        
+
         $logger->addError($request->getQueryString());
-        
+
         $ref = $request->get('Reference', null);
         $actionId = $request->get('Action', 'DETAIL');
         $paramId = $request->get('Param', null);
-        
+
         $logger->addError('$ref: ' . $ref);
         $logger->addError('$actionId: ' . $actionId);
         $logger->addError('$paramId: ' . $paramId);
-        
+
         if (null != $ref && \trim($ref) != '') {
             $em = $this->getEntityManager();
             $order = $em->getRepository('AcfDataBundle:Order')->findOneBy(array(
@@ -63,58 +63,66 @@ class PaymentNotifController extends BaseController
                         $this->gvars['action'] = Order::ST_WAITING;
                         break;
                     case 'ACCORD':
-                        if ($order->getStatus() == Order::ST_NEW || $order->getStatus() == Order::ST_WAITING) {
-                            $order->setStatus(Order::ST_OK);
-                            // paramId à tester ?
-                            $order->setAuth($paramId);
-                            $em->persist($order);
-                            $em->flush();
-                        } elseif ($order->getStatus() != Order::ST_OK) {
-                            // que faire ici ?
-                            throw new HttpException('500', 'Already set Order');
-                        }
+                        // if ($order->getStatus() == Order::ST_NEW || $order->getStatus() == Order::ST_WAITING) {
+                        $order->setStatus(Order::ST_OK);
+                        // paramId à tester ?
+                        $order->setAuth($paramId);
+                        $em->persist($order);
+                        $em->flush();
+                        /*
+                         * } elseif ($order->getStatus() != Order::ST_OK) {
+                         * // que faire ici ?
+                         * throw new HttpException('500', 'Already set Order');
+                         * }//
+                         */
                         $this->gvars['order'] = $order;
                         $this->gvars['action'] = Order::ST_OK;
                         break;
                     case 'REFUS':
-                        if ($order->getStatus() == Order::ST_NEW || $order->getStatus() == Order::ST_WAITING) {
-                            $order->setStatus(Order::ST_REFUSAL);
-                            // paramId à tester ?
-                            $order->setAuth($paramId);
-                            $em->persist($order);
-                            $em->flush();
-                        } elseif ($order->getStatus() != Order::ST_REFUSAL) {
-                            // que faire ici ?
-                            throw new HttpException('500', 'Already set Order');
-                        }
+                        // if ($order->getStatus() == Order::ST_NEW || $order->getStatus() == Order::ST_WAITING) {
+                        $order->setStatus(Order::ST_REFUSAL);
+                        // paramId à tester ?
+                        $order->setAuth($paramId);
+                        $em->persist($order);
+                        $em->flush();
+                        /*
+                         * } elseif ($order->getStatus() != Order::ST_REFUSAL) {
+                         * // que faire ici ?
+                         * throw new HttpException('500', 'Already set Order');
+                         * }//
+                         */
                         $this->gvars['order'] = $order;
                         $this->gvars['action'] = Order::ST_REFUSAL;
                         break;
                     case 'ANNULATION':
-                        if ($order->getStatus() == Order::ST_NEW || $order->getStatus() == Order::ST_WAITING) {
-                            $order->setStatus(Order::ST_CANCELED);
-                            // paramId à tester ?
-                            $order->setAuth($paramId);
-                            $em->persist($order);
-                            $em->flush();
-                        } elseif ($order->getStatus() != Order::ST_CANCELED) {
-                            // que faire ici ?
-                            throw new HttpException('500', 'Already set Order');
-                        }
+                        // if ($order->getStatus() == Order::ST_NEW || $order->getStatus() == Order::ST_WAITING) {
+                        $order->setStatus(Order::ST_CANCELED);
+                        // paramId à tester ?
+                        $order->setAuth($paramId);
+                        $em->persist($order);
+                        $em->flush();
+                        /*
+                         * } elseif ($order->getStatus() != Order::ST_CANCELED) {
+                         * // que faire ici ?
+                         * throw new HttpException('500', 'Already set Order');
+                         * }//
+                         */
                         $this->gvars['order'] = $order;
                         $this->gvars['action'] = Order::ST_CANCELED;
                         break;
                     case 'ERREUR':
-                        if ($order->getStatus() == Order::ST_NEW || $order->getStatus() == Order::ST_WAITING) {
-                            $order->setStatus(Order::ST_ERROR);
-                            // paramId à tester ?
-                            $order->setAuth($paramId);
-                            $em->persist($order);
-                            $em->flush();
-                        } elseif ($order->getStatus() != Order::ST_ERROR) {
-                            // que faire ici ?
-                            throw new HttpException('500', 'Already set Order');
-                        }
+                        // if ($order->getStatus() == Order::ST_NEW || $order->getStatus() == Order::ST_WAITING) {
+                        $order->setStatus(Order::ST_ERROR);
+                        // paramId à tester ?
+                        $order->setAuth($paramId);
+                        $em->persist($order);
+                        $em->flush();
+                        /*
+                         * } elseif ($order->getStatus() != Order::ST_ERROR) {
+                         * // que faire ici ?
+                         * throw new HttpException('500', 'Already set Order');
+                         * }//
+                         */
                         $this->gvars['order'] = $order;
                         $this->gvars['action'] = Order::ST_ERROR;
                         break;
@@ -126,7 +134,7 @@ class PaymentNotifController extends BaseController
         } else {
             throw new HttpException('500', 'Wrong request');
         }
-        
+
         return $this->renderResponse('AcfFrontBundle:Payment:paymentnotif.html.twig', $this->gvars);
     }
 }
